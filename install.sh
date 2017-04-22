7f98f2a45e6ffbe6d9c3a6575bede67daba8e157
Edison Costa <edisoncosta@lojaplus.com.br>
2017-04-22 13:16:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-      #!/bin/bash
+#!/bin/bash
 
-      cd /tmp
-      wget http://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb
-      dpkg -i puppetlabs-release-pc1-jessie.deb
-      apt-get update
+cd /tmp
+wget http://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb
+dpkg -i puppetlabs-release-pc1-jessie.deb
+apt-get update
 
-      apt-get -y install puppet-agent
+apt-get -y install puppet-agent
 
-      echo "PATH=/opt/puppetlabs/bin:/usr/local/bin:$PATH" >>/etc/bash.bashrc
-      echo "export PATH" >>/etc/bash.bashrc
-      export PATH=/opt/puppetlabs/bin:/usr/local/bin:$PATH
+PATH=/opt/puppetlabs/bin:/usr/local/bin:$PATH
       
-      mkdir -p /etc/facter/facts.d
+echo "PATH=$PATH" >>/etc/bash.bashrc
+echo "export PATH" >>/etc/bash.bashrc
+export PATH
+      
+mkdir -p /etc/facter/facts.d
